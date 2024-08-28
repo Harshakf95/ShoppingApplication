89e231fec01ffad0ee9e54bf4f38778bc4f135e7
Harshakf95 <harshakf59@gmail.com>
2024-08-28 17:36:04 +0530

    Commit

---
+++ app/src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+package com.example.shoppingapp
+
+import
+
+class MainActivity : AppCompactActivity() {
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
